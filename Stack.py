f5f67d8c28de559822092c85259b7977bf5f2263
laxmankumar123 <laxmankumar071997@gmail.com>
2024-10-25 14:46:51 +0530

    Stack added on 25oct

---
+++ Stack.py
@@ -0,0 +1,10 @@
+
+In Python, a stack can be implemented using a few different approaches, depending on the requirements. A stack is a data structure that follows the LIFO (Last In, First Out) principle, where the last element added is the first one to be removed.
+
+
+
+
+
+
+
+
